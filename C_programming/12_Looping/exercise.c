e0a337f5815ce57bc4da97026e5406cf325b9044
Karim Ahmed <karim.ahmed14770@gmail.com>
2025-05-09 16:22:51 +0300

    7 problems finished and tested

--- C_programming/12_Looping/exercise.c
+++ C_programming/12_Looping/exercise.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*the best way to solve patterns is visualing it and to have this info
 unsigned char rows;
-unsigned char rows_counter;
-unsigned char cols_counter;
-unsigned char a=0;
+unsigned char rows_index;
+unsigned char cols_index;
+unsigned char mid_index;
+unsigned char cols;
+ready before starting
+ */
+unsigned char rows;
+unsigned char rows_index;
+unsigned char cols_index;
+unsigned char a;
+unsigned char mid_index;
+unsigned char cols;
+
 int main()
 {
     /*******excercise 1***************/
-    printf("enter number of rows\n");
+    printf("excercise 1:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=rows;
+    for(rows_index=0;rows_index<cols;rows_index++)
     {
-        for (cols_counter=0; cols_counter<=rows_counter;cols_counter++)
+        for (cols_index=0; cols_index<=rows_index;cols_index++)
         {
             printf("*");
         }
@@ -21,40 +33,43 @@ int main()
     }
 
     /*******excercise 2***************/
-    printf("enter number of rows\n");
+    printf("excercise 2:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=rows;
+    for(rows_index=0;rows_index<cols;rows_index++)
     {
-        for (cols_counter=0; cols_counter<=rows_counter;cols_counter++)
+        for (cols_index=0; cols_index<=rows_index;cols_index++)
         {
-            printf("%i ",(cols_counter+1));
+            printf("%i ",(cols_index+1));
         }
         printf("\n");
     }
 
 
     /*******excercise 3***************/
-    printf("enter number of rows\n");
+    printf("excercise 3:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=rows;
+    for(rows_index=0;rows_index<cols;rows_index++)
     {
-        for (cols_counter=0; cols_counter<=rows_counter;cols_counter++)
+        for (cols_index=0; cols_index<=rows_index;cols_index++)
         {
-            printf("%c ",((int)'A')+cols_counter);
+            printf("%c ",((int)'A')+cols_index);
         }
         printf("\n");
     }
 
 
     /*******excercise 4***************/
-    printf("enter number of rows\n");
+    printf("excercise 4:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=rows;
+    for(rows_index=0;rows_index<cols;rows_index++)
     {
-        for (cols_counter=rows_counter; cols_counter<rows;cols_counter++)
+        for (cols_index=rows_index; cols_index<rows;cols_index++)
         {
             printf("*");
         }
@@ -62,13 +77,14 @@ int main()
     }
 
     /*******excercise 5***************/
-    printf("enter number of rows\n");
+    printf("excercise 5:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=rows;
+    for(rows_index=0;rows_index<rows;rows_index++)
     {
         a=0;
-        for (cols_counter=rows_counter; cols_counter<rows;cols_counter++)
+        for (cols_index=rows_index; cols_index<cols;cols_index++)
         {
             printf("%i ",++a);
         }
@@ -76,46 +92,55 @@ int main()
     }
 
     /*******excercise 6***************/
-    printf("enter number of rows\n");
+   printf("excercise 6:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    cols=1+(((rows-1)*2)*2);
+    mid_index=cols/2;
+    for(rows_index=0;rows_index<rows;rows_index++)
     {
-        for (cols_counter=0; cols_counter<((rows*2)-1);cols_counter++)
+        for(cols_index=0;cols_index<cols;cols_index++)
         {
-            if((cols_counter>=rows-(rows_counter+1)) && (cols_counter<=rows+(rows_counter-1)))
+            if((cols_index%2==0)&&
+            ((cols_index>=(mid_index-(2*rows_index))) && (cols_index<=(mid_index+(2*rows_index)))))
             {
                 printf("*");
             }
-            else 
-            {
-                printf(" ");
-            }
+            else
+            printf(" ");
         }
         printf("\n");
     }
-
-    /*******excercise 7***************/
-    printf("enter number of rows\n");
+     
+     /*******excercise 7***************/
+   printf("excercise 7:enter number of rows\n");
     fflush(stdin);fflush(stdout);
     scanf("%i",&rows);
+    cols=1+(((rows-1)*2)*2);
+    mid_index=cols/2;
     a=1;
-    unsigned char mid_index=((rows*2)/2)-1;
-    for(rows_counter=0;rows_counter<rows;rows_counter++)
+    for(rows_index=0;rows_index<rows;rows_index++)
     {
-        for (cols_counter=0; cols_counter<((rows*2)-1);cols_counter++)
+        for(cols_index=0;cols_index<cols;cols_index++)
         {
-            if((cols_counter>=rows-(rows_counter+1)) && (cols_counter<=rows+(rows_counter-1)))
+            if((cols_index%2==0)&&
+            ((cols_index>=(mid_index-(2*rows_index))) && (cols_index<=(mid_index+(2*rows_index)))))
             {
-               printf("%i",(a)+(abs((mid_index-cols_counter))));
+                if(cols_index==mid_index)
+                {
+                    printf("%i",a+(2*rows_index));
+                }
+                else
+                {
+                    printf("%i",(a+(2*rows_index))-abs((mid_index-cols_index)/2));   
+                }
             }
-            else 
-            {
-                printf(" ");
-            }
+            else
+            printf(" ");
         }
         printf("\n");
     }
-
+    
     return 0;
 }
+
